6f6fab8befe0fc5af202f3807dfec1a7e59865b3
Karakeen1 <m.moser84@gmx.ch>
2024-11-09 17:31:36 +0100

    Ended Tutorial Video #2

--- flappybird/agent.py
+++ flappybird/agent.py
@@ -1,26 +1,33 @@
+# Tutorial https://www.youtube.com/watch?v=RVMpm86equc
+
 import flappy_bird_gymnasium
 import gymnasium
-import keyboard
 import numpy as np
+import torch
+from dqn import DQN
 
-env = gymnasium.make("FlappyBird-v0", render_mode="human", use_lidar=False)
+device = "cuda" if torch.cuda.is_available() else 'cpu'
 
-obs, _ = env.reset()
-while True:
-    # Next action:
-    # (feed the observation to your agent here)
-    action = env.action_space.sample()
-    if True:
-        if keyboard.is_pressed('space'):
-            action = np.array([1])
-        else:
-            action = np.array([0])
-    # Processing:
-    obs, reward, terminated, _, info = env.step(action)
-    
-    # Checking if the player is still alive
-    if terminated:
-        print(info)
-        break
+class Agent:
+    def run(self, is_training=True, render = False):
+        env = gymnasium.make("FlappyBird-v0", render_mode="human" if render else None, use_lidar=False)
 
-env.close()
+        num_states = env.observation_space.shape[0]
+        num_actions = env.action_space.n 
+        
+        policy_dqn = DQN(num_states, num_actions).to_device(device)
+        
+        obs, _ = env.reset()
+        while True:
+            # Next action:
+            # feed the observation to your agent here)
+            action = env.action_space.sample()
+            # Processing:
+            obs, reward, terminated, _, info = env.step(action)
+        
+            # Checking if the player is still alive
+            if terminated:
+                print(info)
+                break
+
+        env.close()
